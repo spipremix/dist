b92a46cac7fb50541ed0a925ad0354b2907e06ba
tetue <tetue@rezo.net>
2011-09-08 23:07:43 +0000

    Correction de la hiérarchie des titres, qui laissait un trou en h2.

---
+++ mes_fonctions.php
@@ -0,0 +1,8 @@
+<?php
+
+// $GLOBALS['toujours_paragrapher'] = true;
+$GLOBALS['debut_intertitre'] = "\n<h2 class=\"spip\">\n";
+$GLOBALS['fin_intertitre'] = "</h2>\n";
+// $GLOBALS['puce'] = '- ';
+
+?>
