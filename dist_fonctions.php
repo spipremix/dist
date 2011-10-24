ce5486c917574223ff72a761c046eb2875c6f788
eric <eric@smellup.net>
2011-10-24 18:23:12 +0000

    On supprime le fichier des fonctions car il n'y a plus de globale utile.

--- dist_fonctions.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
-// $GLOBALS['toujours_paragrapher'] = true;
-$GLOBALS['debut_intertitre'] = "\n<h2 class=\"spip\">\n";
-$GLOBALS['fin_intertitre'] = "</h2>\n";
-// $GLOBALS['puce'] = '- ';
-
-?>
