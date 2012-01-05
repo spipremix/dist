e97abbceb785077bf83d47f38da617b610e91153
salvatore <salvatore@rezo.net>
2012-01-05 08:09:23 +0000

    langues (paquet-dist)

--- lang/paquet-dist_es.php
+++ lang/paquet-dist_es.php
@@ -1,18 +1,15 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-dist
-// Langue: es
-// Date: 30-07-2011 15:01:36
-// Items: 2
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'dist_description' => 'Juego de esqueletos por defecto en SPIP 3.',
-	'dist_slogan' => 'Juego de esqueletos por defecto en SPIP 3',
+	'dist_slogan' => 'Juego de esqueletos por defecto en SPIP 3'
 );
+
 ?>
