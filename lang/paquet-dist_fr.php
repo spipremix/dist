2647de0ee1b5371f6d9a6f231353a718c70b0534
salvatore <salvatore@rezo.net>
2012-01-05 08:09:23 +0000

    langues (paquet-dist)
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/dist@56547 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/paquet-dist_fr.php
+++ lang/paquet-dist_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-dist
-// Langue: fr
-// Date: 30-07-2011 15:01:36
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/dist/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// D
+	// D
 	'dist_description' => 'Jeu de squelettes distribué avec SPIP 3.',
-	'dist_slogan' => 'Squelettes par défaut de SPIP 3',
+	'dist_slogan' => 'Squelettes par défaut de SPIP 3'
 );
+
 ?>
