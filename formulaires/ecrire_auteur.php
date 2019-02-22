6f9f9bc590404cafdea512901fbcef9835d16f59
Cerdic <cedric@yterium.com>
2019-02-22 16:18:35 +0000

    Report de r114013 : Envoyer des mails en from d'un inconnu c'est de l'usurpation d'identite et ca peut vous conduire au commissariat, en plus d'etre vu comme un SPAM par beaucoup de fournisseur de mail. On laisse donc le from par defaut du site, mais un mets un Reply-To a l'envoyeur et l'email de l'envoyeur suppose (mais pas verifie) en signature du message. C'est plus robuste et moins sujet a litige

--- formulaires/ecrire_auteur.php
+++ formulaires/ecrire_auteur.php
@@ -81,14 +81,22 @@ function formulaires_ecrire_auteur_traiter_dist($id_auteur, $id_article, $mail) 
 		. _T('info_message_2') . ' '
 		. $sujet;
 	$texte = _request('texte_message_auteur');
+	$texte .= "\n-- $adres";
 
 	$texte .= "\n\n-- " . _T('envoi_via_le_site') . ' '
 		. supprimer_tags(extraire_multi($GLOBALS['meta']['nom_site']))
 		. ' (' . $GLOBALS['meta']['adresse_site'] . "/) --\n";
 	$envoyer_mail = charger_fonction('envoyer_mail', 'inc');
 
-	if ($envoyer_mail($mail, $sujet, $texte, $adres,
-		'X-Originating-IP: ' . $GLOBALS['ip'])) {
+	$corps = array(
+		'texte' => $texte,
+		'repondre_a' => $adres,
+		'headers' => array(
+			"X-Originating-IP: " . $GLOBALS['ip'],
+		),
+	);
+
+	if ($envoyer_mail($mail, $sujet, $corps)) {
 		$message = _T('form_prop_message_envoye');
 
 		return array('message_ok' => $message);
